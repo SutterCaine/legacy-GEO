d01f951cb667ccc2747b99ee331221dd04caa63a
SutterCaine <trumpliedpeopledied@protonmail.com>
2024-07-27 19:17:03 -0600

    Geocaching.com people SUCK.

---
+++ README.md
@@ -0,0 +1,44 @@
+GeoCoin 3.2
+
+Simple cryptographic tokens with messaging for sharing and trading!
+
+AS OF BLOCK 12314, The Kimoto Gravity Well has been implemented!
+
+Defaults:
+
+*RPC PORT 3990
+
+*P2P PORT 3991
+
+*TESTNET PORT 13991
+
+This release is wholly different from GeoCoin 1.0 and 2.0.
+
+It includes the following refinements:
+
+New block chain--Yep, I told you it might happen. All old coins
+are now gone, but hey, you can now cache for new ones! With the release
+to the GroundSpeak community I thought it best to adjust and reboot,
+I promise there will not be any further resets of the blockchain for GeoCoin
+post release of 3.0.
+
+Block target times changed to 3 minutes and block maturation to 25 confirms.
+
+Difficulty adjustment has been altered to allow for CPU mining as long as possible.
+
+New Graphics! Program updated with Caching themed icons and lingo.
+
+Built-in Coin Creation System Improvements (Virtual Caching) 
+
+QR Code Support
+
+One-click Access to Command Line Console
+
+Another thing you may notice is the decimals are gone--but really
+that's just trickery. They are still there, but only viewable using
+the Command Line Console for easy math, so values may go up or down
+by a coin or two in your Collected balance.
+
+GeoCoin is Free and Open Source and should not be considered "currency." 
+To that end, 2.5% (25million) of all GeoCoins are pre-cached and held by the 
+developer for use with GeoCoin faucets, giveaways, and other such things. 
---
+++ src/checkpoints.cpp
@@ -0,0 +1,66 @@
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2011-2012 Litecoin Developers
+// Copyright (c) 2013 GeoCoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/assign/list_of.hpp> // for 'map_list_of()'
+#include <boost/foreach.hpp>
+
+#include "checkpoints.h"
+
+#include "main.h"
+#include "uint256.h"
+
+namespace Checkpoints
+{
+    typedef std::map<int, uint256> MapCheckpoints;
+
+    //
+    // What makes a good checkpoint block?
+    // + Is surrounded by blocks with reasonable timestamps
+    //   (no blocks before with a timestamp after, none after with
+    //    timestamp before)
+    // + Contains no strange transactions
+    //
+    static MapCheckpoints mapCheckpoints =
+        boost::assign::map_list_of
+        (     0, hashGenesisBlock)
+        ( 1, uint256("0xf6866df638f56aeefd90e14cc3d8a73ccb00f62f9314f80c0beef774a2499e78"))
+        ( 5, uint256("0xbdac0ccbaf81e5baccf047015458a43d50c99469f48cf34e305594d459dcca90"))
+        ( 10, uint256("0x58142805d8df0db328e6a888f9c431c0eb82c7bb9b28330a28b0b977d695049f"))
+        ( 39, uint256("0x48b6b7a2e9139d4625113276226c14969a7ad8a287b9e15f78f71dc1be49b00a"))
+        ( 8000, uint256("0x530f189f7b9a2848149c03e8e1689e59b4da81eb409b411092b3d12385232ddd"))
+        (12000, uint256("0xa4461ea266967862a5e2dc1bfb57d550970156fa46cd1d19244902083c65b902"))
+        (12315, uint256("0x66a5d017190248cfa4084efc722cb55f189497467133516448552a3f0a6f88f3"))
+;
+    bool CheckBlock(int nHeight, const uint256& hash)
+    {
+        if (fTestNet) return true; // Testnet has no checkpoints
+
+        MapCheckpoints::const_iterator i = mapCheckpoints.find(nHeight);
+        if (i == mapCheckpoints.end()) return true;
+        return hash == i->second;
+    }
+
+    int GetTotalBlocksEstimate()
+    {
+        if (fTestNet) return 0;
+
+        return mapCheckpoints.rbegin()->first;
+    }
+
+    CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
+    {
+        if (fTestNet) return NULL;
+
+        BOOST_REVERSE_FOREACH(const MapCheckpoints::value_type& i, mapCheckpoints)
+        {
+            const uint256& hash = i.second;
+            std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
+            if (t != mapBlockIndex.end())
+                return t->second;
+        }
+        return NULL;
+    }
+}
